e67a7e7cbd08a328c158cb7ccd1d5bd0dd29ad7f
RobSprouse <141681347+RobSprouse@users.noreply.github.com>
2023-11-29 21:43:17 -0500

    Models done?

---
+++ models/Cart.js
@@ -0,0 +1,39 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// TODO: create cart model
+class Cart extends Model {}
+
+Cart.init(
+     {
+          id: {
+               type: DataTypes.INTEGER,
+               allowNull: false,
+               primaryKey: true,
+               autoIncrement: true,
+          },
+          user_id: {
+               type: DataTypes.INTEGER,
+               allowNull: false,
+               references: {
+                    model: 'user',
+                    key: 'id',
+               },
+          },
+          product_id: {
+               type: DataTypes.INTEGER,
+               allowNull: false,
+               references: {
+                    model: 'product',
+                    key: 'id',
+               },
+          },
+     },
+     {
+          sequelize,
+          timestamps: false,
+          freezeTableName: true,
+          underscored: true,
+          modelName: 'cart',
+     }
+);
---
+++ models/Product.js
@@ -0,0 +1,54 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Product extends Model {}
+
+Product.init(
+     {
+          id: {
+               type: DataTypes.INTEGER,
+               allowNull: false,
+               primaryKey: true,
+               autoIncrement: true,
+          },
+          name: {
+               type: DataTypes.STRING,
+               allowNull: false,
+          },
+          description: {
+               type: DataTypes.STRING,
+               allowNull: false,
+          },
+          price: {
+               type: DataTypes.INTEGER,
+               allowNull: false,
+          },
+          stock: {
+               type: DataTypes.INTEGER,
+               allowNull: false,
+          },
+          image_URL: {
+               type: DataTypes.STRING,
+               allowNull: false,
+          },
+          categories: {
+               type: DataTypes.STRING,
+               allowNull: false,
+          },
+          vendor_id: {
+               type: DataTypes.INTEGER,
+               allowNull: false,
+               references: {
+                    model: 'vendor',
+                    key: 'id',
+               },
+          },
+     },
+     {
+          sequelize,
+          timestamps: false,
+          freezeTableName: true,
+          underscored: true,
+          modelName: 'product',
+     }
+);
