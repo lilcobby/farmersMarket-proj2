4c3565b13a8cbea4db3d84321883a7a3ebb35607
Tiffani Underwood <tdunderwood217@gmail.com>
2023-12-02 16:50:53 -0500

    added GET api routes for user and product adjusted route to models to pull data

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const userRoute = require("./userRoute.js");
+const productRoute = require("./productRoute.js");
 
 router.use("/users", userRoute);
+router.use("/products", productRoute);
 
 module.exports = router;
--- controllers/api/userRoute.js
+++ controllers/api/userRoute.js
@@ -1,5 +1,14 @@
 const router = require("express").Router();
-const { User } = require("../../models/User");
+const { User } = require("../../models");
+
+router.get("/", async (req, res) => {
+  try {
+  const userData = await User.findAll();
+  res.status(200).json(userData);
+} catch (err) {
+  res.status(500).json(err);
+} 
+});
 
 router.post("/", async (req, res) => {
   // below line to test route
