4e64be292379a1631a82ff0a6443ccc850e11044
RobSprouse <141681347+RobSprouse@users.noreply.github.com>
2023-12-02 17:37:06 -0500

    cartRoutes, api index, schema, workspace variables, models, seed
    cartRoute.js
         - can get all items in cart, add or update the item and its quantity, and delete one item or all items from cart
         - added finished? cart routes
         - works in insomnia
         - need to add middleware auth and change how the cart is selected based on who the logged in user is
    
    api index.js
         - added the cart route
    
    schema.sql
         - added a sql command to get all of a cart's item, used in dev for routes
    
    workspaceVariables.js
         - added this file to keep track of how values are assigned
    
    product.js
         - had to change URL to url because sequalize will treat it as camel case and insert underscores
    
    package.json
         - added a nodemon watch script
    
    productData.js seed
         - added id's to the products so the name would be consistent with its id

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,11 @@
+// COMMENT: Required Dependencies
 const router = require("express").Router();
 const userRoute = require("./userRoute.js");
+const cartRoute = require("./cartRoute.js");
 
+// COMMENT: Routes after the /api path
 router.use("/users", userRoute);
+router.use("/cart", cartRoute);
 
+// COMMENT: Exports the router
 module.exports = router;
