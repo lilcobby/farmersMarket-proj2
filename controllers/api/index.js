a0935ed9d9007845510cbd71f1f86531b23912e6
RobSprouse <141681347+RobSprouse@users.noreply.github.com>
2023-12-02 17:46:25 -0500

    added tifs files to fix conflicts
    Co-Authored-By: Tunderwood217 <141685686+Tunderwood217@users.noreply.github.com>

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,16 @@
 const router = require("express").Router();
 const userRoute = require("./userRoute.js");
 const cartRoute = require("./cartRoute.js");
+const productRoute = require("./productRoute.js");
 
 // COMMENT: Routes after the /api path
 router.use("/users", userRoute);
 router.use("/cart", cartRoute);
+router.use("/products", productRoute);
 
 // COMMENT: Exports the router
 module.exports = router;
+
+// COMMENT: Required Dependencies
+const router = require("express").Router();
+const userRoute = require("./userRoute.js");
