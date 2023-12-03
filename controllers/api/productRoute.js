457e2a3ec118364b7b69d71b18b6c182d3d67db0
RobSprouse <141681347+RobSprouse@users.noreply.github.com>
2023-12-03 02:59:29 -0500

    added a note to product route and added insomnia file to import to have a copy of the routes i defined

--- controllers/api/productRoute.js
+++ controllers/api/productRoute.js
@@ -2,11 +2,35 @@ const router = require("express").Router();
 const { Product } = require("../../models");
 
 router.get("/", async (req, res) => {
-    try {
-        const productData = await Product.findAll();
-        res.status(200).json(productData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
+     try {
+          const productData = await Product.findAll();
+          res.status(200).json(productData);
+     } catch (err) {
+          res.status(500).json(err);
+     }
 });
 module.exports = router;
+
+/* COMMENT: 
+      made a change to the product table to include a is_active column so that vendors can toggle their products on and off
+      
+      To grab the products that are active to display on the home page, use the following format:
+      
+     router.get("/allActiveProducts", async (req, res) => {
+     try {
+          const productData = await Product.findAll({
+               where: {
+                    is_active: true,
+               },
+          });
+          if (productData.length === 0) {
+               res.status(404).json({ errMessage: "No active products found" });
+               return;
+          }
+          res.status(200).json(productData);
+     } catch (err) {
+          res.status(500).json({ errMessage: err.message });
+     }
+});
+      
+      can see examples of the routes in admin routes and with the insomnia file I put in developmentFiles, copy it then import it to insomnia */
