74647f27d96d85d5f08871b6a2faf8291f68c883
RobSprouse <141681347+RobSprouse@users.noreply.github.com>
2023-12-07 12:53:46 -0500

    save point

--- public/js/editForm.js
+++ public/js/editForm.js
@@ -2,22 +2,22 @@ const toggleBox = document.querySelector("#toggleBox");
 const editForm = document.querySelector("#editForm");
 const normalForm = document.querySelector("#normal");
 toggleBox.addEventListener("change", () => {
-  if (toggleBox.checked) {
-    editForm.style.display = "block";
-  } else {
-    editForm.style.display = "none";
-  }
+     if (toggleBox.checked) {
+          editForm.style.display = "block";
+     } else {
+          editForm.style.display = "none";
+     }
 });
 
 // Add the following code to toggle the display of both elements simultaneously
 toggleBox.addEventListener("change", () => {
-  if (toggleBox.checked) {
-    editForm.style.display = "block";
-    normalForm.style.display = "none";
-  } else {
-    editForm.style.display = "none";
-    normalForm.style.display = "block";
-  }
+     if (toggleBox.checked) {
+          editForm.style.display = "block";
+          normalForm.style.display = "none";
+     } else {
+          editForm.style.display = "none";
+          normalForm.style.display = "block";
+     }
 });
 
 // put request for product forms
@@ -25,62 +25,62 @@ toggleBox.addEventListener("change", () => {
 const forms = document.querySelectorAll("#devView");
 
 const formSubmitHandler = async (event) => {
-  event.preventDefault();
-  const form = event.target;
-  const dataId = parseInt(form.getAttribute("data-id"));
-  const description = form.querySelector(".Desc").value;
-  const name = form.querySelector(".Nam").value;
-  const price = parseFloat(form.querySelector(".Pric").value);
-  const quant = parseInt(form.querySelector(".Quant").value);
+     event.preventDefault();
+     const form = event.target;
+     const dataId = parseInt(form.getAttribute("data-id"));
+     const description = form.querySelector(".Desc").value;
+     const name = form.querySelector(".Nam").value;
+     const price = parseFloat(form.querySelector(".Pric").value);
+     const quant = parseInt(form.querySelector(".Quant").value);
 
-  const response = await fetch(`/api/vendors/products/${dataId}`, {
-    method: "PUT",
-    body: JSON.stringify({
-      name: name,
-      description: description,
-      price: price,
-      stock: quant,
-    }),
-    headers: { "Content-Type": "application/json" },
-  });
-  window.location.reload(true);
+     const response = await fetch(`/api/vendors/products/${dataId}`, {
+          method: "PUT",
+          body: JSON.stringify({
+               name: name,
+               description: description,
+               price: price,
+               stock: quant,
+          }),
+          headers: { "Content-Type": "application/json" },
+     });
+     window.location.reload(true);
 
-  // const data = await response.json();
-  // console.log(data);
+     // const data = await response.json();
+     // console.log(data);
 };
 
 forms.forEach((form) => {
-  form.addEventListener("submit", formSubmitHandler);
+     form.addEventListener("submit", formSubmitHandler);
 });
 
 // new product form
 const newProdForm = document.querySelector("#newProdForm");
 const newProdHandler = async (event) => {
-  event.preventDefault();
+     event.preventDefault();
 
-  const newName = document.querySelector("#newProd-name").value;
-  const newDescription = document.querySelector("#newProd-description").value;
-  const newPrice = document.querySelector("#newProd-price").value;
-  const newQuant = document.querySelector("#newProd-stock").value;
-  const newImg = document.querySelector("#newProd-image").value;
-  // maybe dropdown
-  const newCat = document.querySelector("#newProd-category").value;
+     const newName = document.querySelector("#newProd-name").value;
+     const newDescription = document.querySelector("#newProd-description").value;
+     const newPrice = document.querySelector("#newProd-price").value;
+     const newQuant = document.querySelector("#newProd-stock").value;
+     const newImg = document.querySelector("#newProd-image").value;
+     // maybe dropdown
+     const newCat = document.querySelector("#newProd-category").value;
 
-  const response = await fetch("/api/vendors/addProduct", {
-    method: "POST",
-    body: JSON.stringify({
-      name: newName,
-      description: newDescription,
-      price: newPrice,
-      stock: newQuant,
-      image_url: newImg,
-      category_id: newCat,
-    }),
-    headers: { "Content-Type": "application/json" },
-  });
-  window.location.reload(true);
-  // const data = await response.json();
-  // console.log(data);
+     const response = await fetch("/api/vendors/addProduct", {
+          method: "POST",
+          body: JSON.stringify({
+               name: newName,
+               description: newDescription,
+               price: newPrice,
+               stock: newQuant,
+               image_url: newImg,
+               category_id: newCat,
+          }),
+          headers: { "Content-Type": "application/json" },
+     });
+     window.location.reload(true);
+     // const data = await response.json();
+     // console.log(data);
 };
 
 newProdForm.addEventListener("submit", newProdHandler);
@@ -94,23 +94,23 @@ const vendorBtn = document.querySelector("#vendorInfo");
 
 // function to post
 const addVendInfo = async (event) => {
-  event.preventDefault();
+     event.preventDefault();
 
-  const name = businessName.value;
-  const desc = businessDesc.value;
-  const img = businessImg.value;
+     const name = businessName.value;
+     const desc = businessDesc.value;
+     const img = businessImg.value;
 
-  const response = await fetch("/api/vendors/profile", {
-    method: "PUT",
-    body: JSON.stringify({
-      name: name,
-      description: desc,
-      image_url: img,
-    }),
-    headers: { "Content-Type": "application/json" },
-  });
-  const data = await response.json();
-  console.log(data);
-  // window.location.reload(true);
+     const response = await fetch("/api/vendors/profile", {
+          method: "PUT",
+          body: JSON.stringify({
+               name: name,
+               description: desc,
+               image_url: img,
+          }),
+          headers: { "Content-Type": "application/json" },
+     });
+     const data = await response.json();
+     console.log(data);
+     // window.location.reload(true);
 };
 vendorForm.addEventListener("submit", addVendInfo);
